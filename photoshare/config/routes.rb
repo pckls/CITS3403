71996f05ddc790838f726b17ae39ca51e71a7d5e
Thomas Pickersgill <10315855@ECM-RHL-020.uniwa.uwa.edu.au>
2013-05-19 19:19:49 +0800

    Added controller and view, although not working as yet...

--- photoshare/config/routes.rb
+++ photoshare/config/routes.rb
@@ -1,4 +1,9 @@
 Photoshare::Application.routes.draw do
+
+  get "hello/index"
+  #get "hello"
+  #resources :hello
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -46,4 +51,5 @@ Photoshare::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  #match ':controller(/:action(/:id))(.:format)'
 end
