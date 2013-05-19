c6c74677b4ffdd736cdc39b5512eb85b2aba57de
pickles-it <tom@pickles-it.com.au>
2013-05-19 11:05:46 +0800

    Initial Import

---
+++ photoshare/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Photoshare::Application.config.secret_key_base = 'a7a0dc6439f08272000ccb223f8c8d4bbb6db0aa905956f3f5d9dcd83dbf457711e37c7c8c8e3511e397ff4269ed69bba4abf4c158cbed1e5799647ec8502f67'
